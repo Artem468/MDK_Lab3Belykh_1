7dbac6e6b0c9c9371ce64538728c8b50c8361fc0
Artem <artem.belykh.ru@gmail.com>
2024-02-05 18:50:13 +0200

    Initial commit

---
+++ MDK_Lab3Belykh_1/MainWindow.xaml.cs
@@ -0,0 +1,185 @@
+﻿using MDK_Lab3Belykh_1.Model;
+using System;
+using System.Windows;
+
+
+namespace MDK_Lab3Belykh_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Time? time;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TimeAddOneSec(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                time = new Time(TimeField.Text);
+            }
+            catch (Exception)
+            {
+                time = new Time();
+                TimeField.Text = "0:0:0";
+                MessageBox.Show("Неверные данные\n" +
+                                "Установлено значение по умолчанию");
+            }
+
+            time += new Time(0, 0, 1);
+            TimeField.Text = time.Print();
+        }
+
+        private void HowManySeconds(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                time = new Time(TimeField.Text);
+                MessageBox.Show($"Всего {time.ToSeconds()} секунд");
+            }
+            catch (Exception)
+            {
+                time = new Time();
+                TimeField.Text = time.Print();
+                MessageBox.Show($"Всего {time.ToSeconds()} секунд");
+            }
+        }
+
+        private void HowManyMinutes(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                time = new Time(TimeField.Text);
+                MessageBox.Show($"Всего {time.ToMinutes()} минут");
+            }
+            catch (Exception)
+            {
+                time = new Time();
+                TimeField.Text = time.Print();
+                MessageBox.Show($"Всего {time.ToMinutes()} минут");
+            }
+        }
+
+        private void AddSeconds(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                time = new Time(TimeField.Text);
+                time += new Time(0, 0, Convert.ToInt32(AddSecondField.Text));
+                TimeField.Text = time.Print();
+            }
+            catch (Exception)
+            {
+                time = new Time();
+                TimeField.Text = time.Print();
+                AddSecondField.Text = "0";
+                MessageBox.Show("Неверные данные\n" +
+                                "Установлено значение по умолчанию");
+            }
+        }
+
+        private void SubSeconds(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                time = new Time(TimeField.Text);
+                time -= new Time(0, 0, Convert.ToInt32(SubSecondField.Text));
+                TimeField.Text = time.Print();
+            }
+            catch (Exception)
+            {
+                time = new Time();
+                TimeField.Text = time.Print();
+                SubSecondField.Text = "0";
+                MessageBox.Show("Неверные данные\n" +
+                                "Установлено значение по умолчанию");
+            }
+        }
+
+        private void SubTime(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                time = new Time(TimeField.Text);
+                Time otherTime = new Time(SubTimeField.Text);
+                MessageBox.Show($"Разница в {time.TimeSub(otherTime)}");
+            }
+            catch (Exception)
+            {
+                time = new Time();
+                TimeField.Text = time.Print();
+            }
+        }
+
+        private void IsMoreTime(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                time = new Time(TimeField.Text);
+                Time otherTime = new Time(СompareTimeField.Text);
+                if (time > otherTime)
+                {
+                    MessageBox.Show("Да");
+                }
+                else
+                {
+                    MessageBox.Show("Нет");
+                }
+            }
+            catch (Exception)
+            {
+                time = new Time();
+                TimeField.Text = time.Print();
+            }
+        }
+
+        private void IsLessTime(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                time = new Time(TimeField.Text);
+                Time otherTime = new Time(СompareTimeField.Text);
+                if (time < otherTime)
+                {
+                    MessageBox.Show("Да");
+                }
+                else
+                {
+                    MessageBox.Show("Нет");
+                }
+            }
+            catch (Exception)
+            {
+                time = new Time();
+                TimeField.Text = time.Print();
+            }
+        }
+
+        private void IsEqualTime(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                time = new Time(TimeField.Text);
+                Time otherTime = new Time(СompareTimeField.Text);
+                if (time == otherTime)
+                {
+                    MessageBox.Show("Да");
+                }
+                else
+                {
+                    MessageBox.Show("Нет");
+                }
+            }
+            catch (Exception)
+            {
+                time = new Time();
+                TimeField.Text = time.Print();
+            }
+        }
+    }
+}
---
+++ MDK_Lab3Belykh_1/Model/Time.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace MDK_Lab3Belykh_1.Model
+{
+    public class Time
+    {
+        
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+        
+        public Time()
+        {
+            Hour = 0;
+            Minute = 0;
+            Second = 0;
+        }
+
+        public Time(int hour, int minute, int second)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+
+        public Time(string time)
+        {
+            TimeOnly timeObj = TimeOnly.Parse(time);
+            Hour = timeObj.Hour;
+            Minute = timeObj.Minute;
+            Second = timeObj.Second;
+        }
+
+        public Time(DateTime datetime)
+        {
+            Hour = datetime.Hour;
+            Minute = datetime.Minute;
+            Second = datetime.Second;
+        }
+
+        public Time(long seconds)
+        {
+            TimeSpan time = TimeSpan.FromSeconds(seconds);
+            Hour = time.Hours;
+            Minute = time.Minutes;
+            Second = time.Seconds;
+        }
+        
+        public static Time operator +(Time time1, Time time2)
+        {
+            long time = (time1.Hour * 3600 + time1.Minute * 60 + time1.Second) +
+                        (time2.Hour * 3600 + time2.Minute * 60 + time2.Second);
+            return new Time(time);
+        }
+
+        public static Time operator -(Time time1, Time time2)
+        {
+            long time = (time1.Hour * 3600 + time1.Minute * 60 + time1.Second) -
+                        (time2.Hour * 3600 + time2.Minute * 60 + time2.Second);
+            return new Time(time);
+        }
+
+        public static bool operator >(Time time1, Time time2)
+        {
+            return new TimeSpan(time1.Hour, time1.Minute, time1.Second) > 
+                   new TimeSpan(time2.Hour, time2.Minute, time2.Second);
+        }
+        
+        public static bool operator <(Time time1, Time time2)
+        {
+            return new TimeSpan(time1.Hour, time1.Minute, time1.Second) < 
+                   new TimeSpan(time2.Hour, time2.Minute, time2.Second);
+        }
+        
+        public static bool operator ==(Time time1, Time time2)
+        {
+            return new TimeSpan(time1.Hour, time1.Minute, time1.Second) == 
+                   new TimeSpan(time2.Hour, time2.Minute, time2.Second);
+        }
+        
+        public static bool operator !=(Time time1, Time time2)
+        {
+            return new TimeSpan(time1.Hour, time1.Minute, time1.Second) != 
+                   new TimeSpan(time2.Hour, time2.Minute, time2.Second);
+        }
+        
+        public string Print()
+        {
+            return $"{Hour}:{Minute}:{Second}";
+        }
+        
+        public double TimeSub(Time obj)
+        {
+            TimeOnly time = new TimeOnly(Hour, Minute, Second);
+            TimeOnly timeObj = new TimeOnly(obj.Hour, obj.Minute, obj.Second);
+            return (time - timeObj).TotalSeconds;
+        }
+        
+        public double ToSeconds()
+        {
+            TimeSpan time = new TimeSpan(Hour, Minute, Second);
+            return double.Round(time.TotalSeconds);
+        }
+
+        public double ToMinutes()
+        {
+            TimeSpan time = new TimeSpan(Hour, Minute, Second);
+            return double.Round(time.TotalMinutes);
+        }
+        
+    }
+}
